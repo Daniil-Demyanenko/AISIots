71ee919ba38da089bab0e701f7d0b8a14c32f9a8
Daniil Demyanenko <daniil.demyanenko1@gmail.com>
2024-05-12 22:50:13 +0300

    Свойства таблиц Plans.PlanBlocks

--- DAL/SqliteContext.cs
+++ DAL/SqliteContext.cs
@@ -1,5 +1,6 @@
 using AISIots.Models.DbTables;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace AISIots.DAL;
 
@@ -11,5 +12,20 @@ public sealed class SqliteContext : DbContext
     public SqliteContext(DbContextOptions<SqliteContext> options) : base(options)
     {
         Database.EnsureCreated();
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new RpdConfiguration());
+        
+        modelBuilder.Entity<PlanBlock>()
+            .HasMany(p => p.BlockSections)
+            .WithOne()
+            .HasForeignKey(b => b.Id);
+
+        modelBuilder.Entity<BlockSection>()
+            .HasMany(s => s.ShortRpds)
+            .WithOne()
+            .HasForeignKey(s => s.Id);
     }
 }
--- Models/DbTables/PlanBlock.cs
+++ Models/DbTables/PlanBlock.cs
@@ -1,21 +1,24 @@
-using DocumentFormat.OpenXml.Office2010.ExcelAc;
+using System.ComponentModel.DataAnnotations;
 
 namespace AISIots.Models.DbTables;
 
 public class PlanBlock(string title)
 {
+    [Key] public int Id { get; set; }
     public string Title { get; set; } = title;
     public List<BlockSection> BlockSections { get; set; } = new();
 }
+
 public class BlockSection(string title)
 {
+    [Key] public int Id { get; set; }
     public string Title { get; set; } = title;
     public List<ShortRpd> ShortRpds { get; set; } = new();
-    
 }
 
 public class ShortRpd(string discipline, string index)
 {
+    [Key] public int Id { get; set; }
     public string Discipline { get; set; } = discipline;
     public string Index { get; set; } = index;
 }
