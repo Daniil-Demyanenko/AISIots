2bbf92099776b42a5ecb02180831a09dc330c52d
Daniil Demyanenko <daniil.demyanenko1@gmail.com>
2024-08-07 10:06:48 +0300

    удалён dbSearch

--- Models/SearchModel.cs
+++ Models/SearchModel.cs
@@ -1,5 +1,5 @@
 using AISIots.DAL;
-using AISIots.Services;
+using FuzzySharp;
 
 namespace AISIots.Models;
 
@@ -9,14 +9,30 @@ public class SearchModel(IEnumerable<SearchItem> items, bool isRpdSearch)
     public bool IsRpdSearch = isRpdSearch;
 
     public static SearchModel Create(SqliteContext db, string? searchString = null, bool isRpdSearch = true)
+        => string.IsNullOrEmpty(searchString?.Trim()) ? GetNewestRpds(db) : GetFuzzySorted(searchString, isRpdSearch, db);
+
+
+    private static SearchModel GetNewestRpds(SqliteContext db)
     {
-        var searcher = new DbSearch(db);
-
-        if (string.IsNullOrEmpty(searchString?.Trim()))
-            return searcher.GetNewestRpds();
-
-        return searcher.GetFuzzySorted(searchString, isRpdSearch);
+        var result = db.Rpds
+            .OrderByDescending(x => x.UpdateDateTime).Take(50)
+            .Select(x => new SearchItem(x.Id, x.Title));
+        return new SearchModel(result, isRpdSearch: true);
     }
+
+    private static SearchModel GetFuzzySorted(string searchString, bool isRpdSearch, SqliteContext db)
+    {
+        var select = isRpdSearch
+            ? db.Rpds.Select(x => new SearchItem(x.Id, x.Title))
+            : db.Plans.Select(x => new SearchItem(x.Id, $"{GetFirstPart(x.Code)} - {x.Profile} ({x.GroupYear})"));
+
+        var items = select.AsEnumerable().OrderByDescending(x => Fuzz.TokenSortRatio(searchString.ToLower(), x.Title.ToLower())).Take(50);
+
+        return new SearchModel(items, isRpdSearch);
+    }
+
+    private static string GetFirstPart(string? str)
+        => str?.Split(' ')[0] ?? "";
 }
 
 public record SearchItem(int Id, string Title);
